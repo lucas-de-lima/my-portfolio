932cc36583445800e7d56396fb40f2cc4cc11368
Lucas-De-Lima <lucasramosdelima@gmail.com>
2022-12-24 15:08:26 -0300

    encerrando trabalho em "About"

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -16,13 +16,7 @@ function About() {
   return (
     <>
     <div>
-      <h2 className="head-text">
-        I Know that
-        <span>Good Design</span>
-        <br />
-        means
-        <span>Good Business</span>
-      </h2>
+      <h2 className="head-text">I Know that <span>Good Development</span> <br/> means <span>Good Business</span></h2>
 
       <div className="app__profiles" />
         {abouts.map( (about, index) => (
