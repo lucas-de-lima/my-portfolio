a8f5de22424d783d5fc3712eb90019acf3285864
Lucas-De-Lima <lucasramosdelima@gmail.com>
2022-12-24 17:37:23 -0300

    feat:"AppWrap.js"

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { NavigationDots, SocialMedia } from '../components';
 
-function AppWrap() {
+
+function AppWrap(Component, idName, classNames) {
+  return function HOC() {
   return (
-    <div>AppWrap</div>
+    <div id={idName} className={`app__container ${classNames}`}>
+      <SocialMedia />
+
+      <div className='app__wrapper app__flex'>
+        <Component />
+
+        <div className="copyright">
+          <p className="p-text">@2022 LUCAS</p>
+          <p className="p-text">All rights reserved</p>
+        </div>
+      </div>
+      <NavigationDots active={idName} />
+    </div>
   )
+}
 };
+
+AppWrap.propTypes = {}.isRequired;
 
 export default AppWrap;
