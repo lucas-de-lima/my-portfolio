5876ed5396aca70587cb5e13dc2419b43053f524
Lucas-De-Lima <lucasramosdelima@gmail.com>
2022-12-25 11:23:04 -0300

    inicialdo trabalho nesta seção

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
 
-import './Skills.scss'
+
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
+import './Skills.scss';
 
 
 function Skills() {
