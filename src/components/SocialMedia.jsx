911b4d6e3be87b2fd1567bfe19db07381145de22
Lucas-De-Lima <lucasramosdelima@gmail.com>
2022-12-27 17:48:40 -0300

    fix: colocando link das redes sociais

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -6,13 +6,28 @@ function SocialMedia() {
   return (
     <div className="app__social">
         <div>
-            <BsTwitter />
+            <a 
+              href="https://twitter.com/LucasDeLima___" 
+              target="_blank" 
+              rel="noopener noreferrer">
+                <BsTwitter />
+            </a>
         </div>
         <div>
-            <FaFacebookF />
+            <a 
+              href="https://www.facebook.com/profile.php?id=100089004522410" 
+              target="_blank" 
+              rel="noopener noreferrer">
+                <FaFacebookF /> 
+            </a>
         </div>
         <div>
-            <BsInstagram />
+            <a 
+              href="https://www.instagram.com/dev.lucasdelima/"      
+              target="_blank" 
+              rel="noopener noreferrer">
+                <BsInstagram />
+            </a>
         </div>
     </div>
   )
